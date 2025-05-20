04b5ca580d11af875f3d8f43dcb992058552463a
Rue <rokki1021@163.com>
2025-05-20 11:57:08 +0800

    Initial commit

---
+++ rpc-demo/rpc-api/src/main/java/com/ruerpc/Hello.java
@@ -0,0 +1,8 @@
+package com.ruerpc;
+
+/**
+*   @author Rue
+*   @date 2025/5/19 21:30
+*/
+public interface Hello {
+}
---
+++ rpc-demo/rpc-provider-demo/src/main/java/com/ruerpc/Impl/HelloImpl.java
@@ -0,0 +1,8 @@
+package com.ruerpc.Impl;
+
+/**
+*   @author Rue
+*   @date 2025/5/19 21:35
+*/
+public class HelloImpl {
+}
